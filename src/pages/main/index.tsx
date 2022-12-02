20046f06e421daf528e059ffde21fd4daefffed5
Mahdi Rashidi <wis-21@Mahdis-MacBook-Pro.local>
2022-12-02 19:50:16 +0330

    build a first section of main page

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
-
+import React from "react";
+import Form from "@components/form";
 export default function Main() {
   return (
-    <div>Main</div>
-  )
+    <div>
+      <section className="pt-24 lg:pb-4 bg-red-300 flex flex-col items-center">
+        <div className="uppercase text-2xl font-bold">
+          to do list
+        </div>
+        
+      <Form />
+      </section>
+    </div>
+  );
 }
