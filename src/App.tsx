4e7291f647a94e85056c9331e73464b3ff99c7fa
Mahdi Rashidi <wis-21@Mahdis-MacBook-Pro.local>
2022-12-02 14:54:29 +0330

    fix config folder struction of project

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import Main from "./pages/main";
 
 function App() {
   return (
-    <div className="App">
-      <header className="bg-red-600">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Main />
+    </>
   );
 }
 
