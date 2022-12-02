36863e77d58c7987a05f507b5cb6f12279ab3ecd
Mahdi Rashidi <wis-21@Mahdis-MacBook-Pro.local>
2022-12-02 19:47:01 +0330

    fix create a appInput component

--- src/components/appInput/index.tsx
+++ src/components/appInput/index.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
-
-export default function AppInput() {
+import React from "react";
+import { appInputPropsType } from "./types";
+export default function AppInput({
+  className,
+  isInvalid,
+  ...props
+}: appInputPropsType) {
   return (
-    <div>
-      <input type="text" />
+    <div className=" mb-6 lg:mb-0 w-full">
+      <div
+        className={`flex flex-col my-2 pl-1 lg:my-0  ${className} ${
+          isInvalid ? "animate-shake border-2 border-red-500" : ""
+        }`}
+      >
+        <input
+          className={`lg:my-0 pl-1 lg:py outline-0 w-full h-full `}
+          {...props}
+          required
+        />
+      </div>
+      {isInvalid && (
+        <div className={`text-red-500 font-bold text-xs`}>
+          please fill the input!!!
+        </div>
+      )}
     </div>
-  )
+  );
 }
--- src/components/appInput/types.ts
+++ src/components/appInput/types.ts
@@ -1,1 +1,11 @@
-export const x = {}
+import {ChangeEvent} from "react"
+
+export type appInputPropsType = {
+    type: string,
+    name: string,
+    placeholder: string,
+    className?: string,
+    isInvalid?: boolean,
+    value: string,
+    onChange: (e: ChangeEvent<HTMLInputElement>) => void
+}
