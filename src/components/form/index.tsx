72da6b6a67a5f7a5fa01410902a3c054288879e2
Mahdi Rashidi <wis-21@Mahdis-MacBook-Pro.local>
2022-12-04 00:18:43 +0330

    fix remove useless config

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -1,28 +1,22 @@
-import { useState } from "react";
-import { formEvent, inputEvent } from "./types";
+import { inputEvent } from "./types";
 import AppInput from "@components/appInput";
 import AppButton from "@components/appButton";
+import { Context } from "@context/index";
+import { useContext } from "react";
 export default function Form() {
-  const [isInvalid, setIsInvalid] = useState(false);
-  const [todoVal, setTodoVal] = useState("");
-  const handleSubmit = (e: formEvent) => {
-    e.preventDefault();
-    alert(todoVal.trim());
-    if (!todoVal.trim().length) setIsInvalid(true);
-    else {
-      setIsInvalid(false);
-    }
-  };
+  const { createTodo, todo, handleOnChange, isInvalid } =
+    useContext(Context);
   const handleChange = (e: inputEvent) => {
-    setTodoVal(e.target?.value);
+    handleOnChange(e.target.value);
   };
   return (
     <div className="px-3 w-full">
       <form
         onSubmit={(e) => {
-          handleSubmit(e);
+          e.preventDefault();
+          createTodo(todo);
         }}
-        className="lg:flex justify-center lg:h-[3.5rem] my-8 w-6/12 mx-auto"
+        className="lg:flex justify-center lg:h-[3.5rem] my-8 w-full md:w-6/12 mx-auto"
       >
         <AppInput
           type="text"
@@ -30,12 +24,12 @@ export default function Form() {
           placeholder="Title..."
           className="rounded-none w-full flex-grow bg-white h-full py-4 lg:py-0"
           isInvalid={isInvalid}
-          value={todoVal}
+          value={todo}
           onChange={handleChange}
         />
         <AppButton
           type="submit"
-          className="h-full capitalize bg-slate-300 py-4 lg:py-0"
+          className="h-full capitalize bg-slate-300 py-4 lg:py-0 lg:px-16"
         >
           submit
         </AppButton>
