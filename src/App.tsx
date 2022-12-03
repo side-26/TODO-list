03a7bff5f49e68f206c6536ccbb5d734b9d48af1
Mahdi Rashidi <wis-21@Mahdis-MacBook-Pro.local>
2022-12-04 00:24:25 +0330

    fix transfar contextApi to root

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,50 @@
-import React from "react";
-
-import TodoList from "./pages/todoList";
-
+import TodoList from "@pages/todoList";
+import FooterLayout from "@layouts/footer";
+import { Context } from "@context/index";
+import { useTodoList } from "@hooks/useTodoList";
 function App() {
+  const {
+    todo,
+    isInvalid,
+    todosArr,
+    setActiveTodo,
+    setCreateTodo,
+    setDeleteCompletedTodos,
+    setDeleteTodo,
+    activeTodosArrLen,
+    setOnChange,
+    activeTodosArr,
+    setshowAllTodosState,
+    setShowCompletedTodos,
+    setShowAllTodos,
+    showAllTodosState,
+    isEmpty,
+  } = useTodoList();
   return (
-    <>
-      <TodoList />
-    </>
+    <Context.Provider
+      value={{
+        todo,
+        isInvalid,
+        todosArr,
+        activeTodosArr: activeTodosArr,
+        createTodo: setCreateTodo,
+        deleteTodo: setDeleteTodo,
+        handleActiveTodo: setActiveTodo,
+        handleShowingAllTodos: setShowAllTodos,
+        deleteCompletedTodos: setDeleteCompletedTodos,
+        showActiveTodos: setShowCompletedTodos,
+        handleOnChange: setOnChange,
+        showAllTodosState,
+        activeTodosArrLen,
+        isEmpty,
+      }}
+    >
+      <>
+        <FooterLayout>
+          <TodoList />
+        </FooterLayout>
+      </>
+    </Context.Provider>
   );
 }
 
