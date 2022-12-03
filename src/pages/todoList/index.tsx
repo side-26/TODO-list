1275b7549fbc9795b9fd253af54c1f69282c2067
Mahdi Rashidi <wis-21@Mahdis-MacBook-Pro.local>
2022-12-04 00:21:00 +0330

    fix remove contextApi in todoList page

--- src/pages/todoList/index.tsx
+++ src/pages/todoList/index.tsx
@@ -1,37 +1,24 @@
-import { useState } from "react";
 import Form from "@components/form";
 import { Context } from "@context/index";
-import { useTodoList } from "@hooks/useTodoList";
+import TodoListSec from '@components/todoList';
+import { useMeta } from "@hooks/useMeta";
+import {useContext} from 'react';
 export default function TodoList() {
-  const {
-    todo,
-    isInvalid,
-    todosArr,
-    setActiveTodo,
-    setCreateTodo,
-    setDeleteAllTodos,
-    setDeleteTodo,
-    setOnChange,
-  } = useTodoList();
+  const [setTitle] = useMeta();
+  const {todo}=useContext(Context)
   
+  setTitle("todo-list");
   return (
-    <Context.Provider
-      value={{
-        todo,
-        isInvalid,
-        todosArr,
-        setTodo: setCreateTodo,
-        deleteTodo: setDeleteTodo,
-        handleActiveTodo: setActiveTodo,
-        deleteAllTodo: setDeleteAllTodos,
-      }}
-    >
       <div>
         <section className="pt-24 lg:pb-4 bg-red-300 flex flex-col items-center">
           <div className="uppercase text-2xl font-bold">to do list</div>
+          <div className="uppercase text-2xl font-bold">{todo}</div>
           <Form />
         </section>
+        <section>
+          <TodoListSec/>
+        </section>
       </div>
-    </Context.Provider>
+
   );
 }
