490bd4f419d797842de15724de5482589b6c78e1
Mahdi Rashidi <wis-21@Mahdis-MacBook-Pro.local>
2022-12-04 00:19:20 +0330

    fix config todoList section

--- src/components/todoList/index.tsx
+++ src/components/todoList/index.tsx
@@ -1,7 +1,47 @@
-import React from 'react'
+import TodoItem from "@components/todoItem";
+import { Context } from "@context/index";
+import { useContext, useEffect, useState } from "react";
+import { todoArrType } from "@context/todo/types";
+import "./style.css";
+export default function TodoListSec() {
+  const {
+    todosArr,
+    deleteTodo,
+    handleActiveTodo,
+    isEmpty,
+    showAllTodosState,
+    activeTodosArr,
+  } = useContext(Context);
+  const [todoArr, setTodoArr] = useState<any>([]);
+  useEffect(() => {
+    if (showAllTodosState) setTodoArr([...todosArr]);
+    else setTodoArr([...activeTodosArr]);
+  }, [showAllTodosState, todosArr,activeTodosArr]);
 
-export default function TodoList() {
   return (
-    <div>TodoList</div>
-  )
+    <section className="">
+      <div className="text-gray-300 p-3 lg:px-10 xl:px-14">
+        What needs to be done?
+      </div>
+      <div>{showAllTodosState}</div>
+      <div>
+        {!todoArr.length ? (
+          <div className="typing-demo mx-auto">there is no data to show!</div>
+        ) : (
+          todoArr.map((todo: any,) => (
+            <div>
+              <TodoItem
+                id={todo.id}
+                isChecked={todo.isChecked}
+                todoName={todo.todoName}
+                handleCheckItem={handleActiveTodo}
+                handleDeleteItem={deleteTodo}
+                key={todo.id}
+              />
+            </div>
+          ))
+        )}
+      </div>
+    </section>
+  );
 }
